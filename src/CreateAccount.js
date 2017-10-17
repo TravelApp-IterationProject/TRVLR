d42d23f1cfb0486c97814c6a53b734577707b940
Jelena Gruica <jelena.gruica@gmail.com>
2017-10-17 10:55:42 -0700

    created signup page and putting created user in db

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -1,34 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Login from './Login.js';
+import { Route, Redirect } from 'react-router'
 
-var that;
 class CreateAccount extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      username: '',
+      password: '',
+      isSignedIn: null
     };
     this.create = this.create.bind(this)
+    this.usernameChanged = this.usernameChanged.bind(this)
+    this.passwordChanged = this.passwordChanged.bind(this)
   }
 
-  create() {
+  create(e) {
     //plugin get request here
+     e.preventDefault()
+    const { username, password } = this.state
+    if (username && password) {
+      axios.post('/api/signup', {
+        username,
+        password
+      })
+      .then(() => {
+        this.setState({ isSignedIn: true })
+      })
+      .catch(() => {
+        this.setState({ isSignedIn: false })
+      });
+    }
     console.log("logging in!");
   }
 
+  usernameChanged(e) {
+    const { value } = e.target
+    this.setState(state => ({ username: value }))
+  }
+
+  passwordChanged(e) {
+    const { value } = e.target
+    this.setState(state => ({ password: value }))
+  }
+
   render() {
+    const { username, password, isSignedIn } = this.state
     return (
       <div className='routed-component'>
         <div className="login-container">
         <h1>Create Account</h1>
         <form onSubmit={this.create}>
-        <input name="username" type="text" placeholder="username"></input>
-        <input name="password" type="password" placeholder="password"></input>
-        <input type='submit' value="login"></input>
+        <input name="username" type="text" placeholder="username" value={username} onChange={this.usernameChanged}></input>
+        <input name="password" type="password" placeholder="password" value={password} onChange={this.passwordChanged}></input>
+        <button className='loginBtn' onClick={this.create}>Create account</button>
         </form>
-       
-           <Link to='/'>Login</Link>
+          {(isSignedIn === true) &&  <Redirect to='/'/>}
         </div>
       </div>
    
@@ -36,10 +67,3 @@ class CreateAccount extends React.Component {
   }
 }
 export default CreateAccount;
-
-  // <form method="POST" action='/login'>
-  //   <input name="username" type="text" placeholder="username"></input>
-  //   <input name="password" type="password" placeholder="password"></input>
-  //   <input type='submit' value="login">
-  // </form>
-  // <a href='./signup'>Sign up</a>
--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import Bigtable from './Bigtable.js';
 import { Route, Redirect } from 'react-router'
+import axios from 'axios';
 
 class Login extends React.Component {
 
@@ -59,7 +60,7 @@ class Login extends React.Component {
           <Link to='/CreateAccount'>Create Account</Link>
 
           {(isSignedIn === true) &&  <Redirect to='/Bigtable'/>}
-          {(isSignedIn === false) && <p>Username and password combination is invalid.</p>}
+          {(isSignedIn === false) && <p>Username and password combination is invalid</p>}
         </div>
       </div>
     )
